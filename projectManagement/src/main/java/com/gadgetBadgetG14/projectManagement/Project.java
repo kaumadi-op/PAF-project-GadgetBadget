fbeb23703e0e9c30bd46b3ab8e638864657660f6
Kaumadi <k.omesha99@gmail.com>
2021-04-20 01:41:48 +0530

    Project class is created.

---
+++ projectManagement/src/main/java/com/gadgetBadgetG14/projectManagement/Project.java
@@ -0,0 +1,37 @@
+package com.gadgetBadgetG14.projectManagement;
+
+public class Project {
+	
+	public int p_id;
+	public String p_name;
+	public String description;
+	public int researcher_id;
+	public int getP_id() {
+		return p_id;
+	}
+	public void setP_id(int p_id) {
+		this.p_id = p_id;
+	}
+	public String getP_name() {
+		return p_name;
+	}
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getResearcher_id() {
+		return researcher_id;
+	}
+	public void setResearcher_id(int researcher_id) {
+		this.researcher_id = researcher_id;
+	}
+	
+	
+	
+
+}
