65a8821e6b63e37123dfbcbc1c706c6d73602c49
kaumadi-op <kaumadipathinayake@gmail.com>
2021-04-20 02:55:59 +0530

    ProjectResources class is edited

--- projectManagement/src/main/java/com/gadgetBadgetG14/projectManagement/ProjectResource.java
+++ projectManagement/src/main/java/com/gadgetBadgetG14/projectManagement/ProjectResource.java
@@ -1,5 +1,44 @@
 package com.gadgetBadgetG14.projectManagement;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/project")
+
 public class ProjectResource {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Project> getAllprojects() {
+		
+		List<Project> projects = new ArrayList<Project>(); 
+        
+		Project p1 = new Project();
+		p1.setP_id(1);
+		p1.setP_name("Kaumadi");
+		p1.setDescription("Abcd");
+		
+		projects.add(p1);
+		
+		return projects;
+    }
+	
+	
+	@POST
+	@Path("/project")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Project createProject(Project p1) {
+		System.out.println(p1);
+		return p1;
+	}
 
 }
