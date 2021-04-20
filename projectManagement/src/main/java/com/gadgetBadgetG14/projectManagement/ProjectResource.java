c5f0c2cd1c26677b96fc2f3272b38a48e768dfec
kaumadi-op <kaumadipathinayake@gmail.com>
2021-04-20 14:54:20 +0530

    Insert operation is implemented.

--- projectManagement/src/main/java/com/gadgetBadgetG14/projectManagement/ProjectResource.java
+++ projectManagement/src/main/java/com/gadgetBadgetG14/projectManagement/ProjectResource.java
@@ -9,27 +9,21 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/project")
+@Path("/projects")
 
 public class ProjectResource {
 	
+	ProjectRepository pr = new ProjectRepository();
+	
 	@GET
+	
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Project> getAllprojects() {
-		
-		List<Project> projects = new ArrayList<Project>(); 
-        
-		Project p1 = new Project();
-		p1.setP_id(1);
-		p1.setP_name("Kaumadi");
-		p1.setDescription("Abcd");
-		
-		projects.add(p1);
-		
-		return projects;
+		return pr.getAllProjects();
     }
 	
 	
@@ -37,8 +31,9 @@ public class ProjectResource {
 	@Path("/project")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Project createProject(Project p1) {
-		System.out.println(p1);
-		return p1;
+		return pr.createProject(p1);
 	}
+	
+	
 
 }
