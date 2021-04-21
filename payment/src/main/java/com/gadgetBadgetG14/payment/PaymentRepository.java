4353d2c8314b4d7e36fdc373083e48db38c39144
danuri99 <68706157+danuri99@users.noreply.github.com>
2021-04-22 00:53:31 +0530

    Update PaymentRepository.java

--- payment/src/main/java/com/gadgetBadgetG14/payment/PaymentRepository.java
+++ payment/src/main/java/com/gadgetBadgetG14/payment/PaymentRepository.java
@@ -5,11 +5,9 @@ import java.util.List;
 import java.sql.*;
 
 public class PaymentRepository {
-
-	Connection con = null;
-	
-	List<Payment> payments;
-	public PaymentRepository() {
+	public Connection getConnection() {
+		
+		Connection con = null;
 		
 		String url = "Jdbc:mysql://localhost:3307/paymentapi";
 		String username = "root";
@@ -20,8 +18,14 @@ public class PaymentRepository {
 		}catch (Exception e) {
 			System.out.println(e);
 		}
-		
-		
+		System.out.println("Successful");
+		return con;
+	}
+	
+	
+	List<Payment> payments;
+	public PaymentRepository() {
+			
 		payments = new ArrayList<>();
 		
 		Payment p1 = new Payment();
@@ -40,13 +44,16 @@ public class PaymentRepository {
 		
 		String insertSql = "INSERT INTO `payment`(`paymentID`, `Amount`, `paymentType`) VALUES (?,?,?)";
 		
+		Connection con = getConnection();
 		try {
-			PreparedStatement st = con.prepareStatement(insertSql);
-			st.setInt(1, p1.paymentId);
-			st.setInt(2, p1.amount);
-			st.setString(3, p1.payType);
+			PreparedStatement pst = con.prepareStatement(insertSql);
+			pst.setInt(1, p1.paymentId);
+			pst.setInt(2, p1.amount);
+			pst.setString(3, p1.payType);
 			
-			st.executeUpdate();
+			pst.executeUpdate();
+			
+			String output = "Inserted Successfully";
 			
 		}catch (Exception e) {
 			System.out.println(e);
@@ -54,5 +61,64 @@ public class PaymentRepository {
 		payments.add(p1);
 		System.out.println(payments);
 		return p1;
+	}
+	
+	public Payment getpaymentId(int paymentId) {
+		String getsql = "SELECT * FROM `payment` WHERE paymentId = '"+paymentId+"'";
+		Payment pd = new Payment();
+		Connection con = getConnection();
+		
+		try {
+			Statement s = con.createStatement();
+			ResultSet rs = s.executeQuery(getsql);
+			
+			while(rs.next()) {
+				
+				pd.setPaymentId(rs.getInt(0));
+				pd.setAmount(rs.getInt(1));
+				pd.setPayType(rs.getString(2));
+				
+			}
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return pd;
+	}
+	
+	public String deletePayment(int paymentId) {
+		
+		String output = "";
+		
+		try {
+			Connection con = getConnection();
+			
+			String deletePayment = "DELETE FROM `payment` WHERE paymentId = '"+paymentId+"' ";
+			PreparedStatement pst = con.prepareStatement(deletePayment);
+			pst.execute();
+			
+			output = "Successfully Deleted";
+			con.close();
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return output;
+	}
+	
+	public void updatePayment(Payment payment) {
+		
+		try {
+			Connection con = getConnection();
+			
+			String updatePayment = "UPDATE `payment` SET `paymentID`='"+payment.getPaymentId()+"',`Amount`='"+payment.getAmount()+"',`paymentType`='"+payment.getPayType()+"' WHERE paymentID = '"+payment.getPaymentId()+"'";
+			PreparedStatement st = con.prepareStatement(updatePayment);
+			
+			st.executeUpdate();
+			
+			con.close();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
