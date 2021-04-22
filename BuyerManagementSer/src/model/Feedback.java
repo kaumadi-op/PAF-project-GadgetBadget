857a52ca82d3454f536bca5dbd5d9c3472e2ace9
isuri98 <isuritissera98@gmail.com>
2021-04-22 20:51:54 +0530

    Update feedback class and web.xml

--- BuyerManagementSer/src/model/Feedback.java
+++ BuyerManagementSer/src/model/Feedback.java
@@ -19,7 +19,7 @@ public class Feedback {
 			 Class.forName("com.mysql.jdbc.Driver"); 
 			 
 			 //Provide the correct details: DBServer/DBName, username, password 
-			 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/buyer useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", ""); 
+			 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/buyer? useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", ""); 
 			 } 
 			 catch (Exception e) 
 			 {e.printStackTrace();} 
@@ -35,7 +35,7 @@ public class Feedback {
 			 {return "Error while connecting to the database for inserting."; } 
 			 // create a prepared statement
 			 String query = " insert into feedback (`fedID`,`name`,`contactNo`,`email`,`comment`,`ratetype`)"
-			 + " values (?, ?, ?, ?, ?)"; 
+			 + " values (?, ?, ?, ?, ?,?)"; 
 			 PreparedStatement preparedStmt = con.prepareStatement(query); 
 			 // binding values
 			 //preparedStmt.setInt(1, 0); 
